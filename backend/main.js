bf2fb88fc2748407a8352d9dcb9cbcd73669d7e6
Gabriel Bustamante <gabriel.bustamante04@inacapmail.cl>
2024-07-16 18:02:48 -0400

    perfil add

--- backend/main.js
+++ backend/main.js
@@ -5,3 +5,37 @@ function authUser(){
         window.location.href = '../frontend/admin.html';
     }
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const data = [
+        {Nombre: 'Gabriel', Apellido: 'Bustamante'}
+    ];
+
+    function popularTabla (data) {
+        const tableBody = document.getElementById('cuerpoTablaAdmin');
+        data.forEach(item => {
+            const row = document.createElement('tr');
+
+            const celdaNombre = document.createElement('td');
+            celdaNombre.textContent = item.Nombre;
+            row.appendChild(celdaNombre);
+
+            const celdaApellido = document.createElement('td');
+            celdaApellido.textContent = item.Apellido;
+            row.appendChild(celdaApellido);
+
+            const celdaBoton = document.createElement('td');
+            const button = document.createElement('button');
+            button.textContent = 'Editar';
+            button.className  = 'btn btn-primary';
+            celdaBoton.appendChild(button);
+            row.appendChild(celdaBoton);
+
+            tableBody.appendChild(row);
+        });
+    }
+
+    popularTabla(data);
+
+
+})
